8ba38184fbc44c2682a9d079a338381a6c4dae5c
sozemego <piknypiec@gmail.com>
2017-02-26 16:44:22 +0100

    Added EventHandler interface for publish-subscribe type communication and added one implementing classes (using guava's EventBus)

---
+++ server/src/main/java/soze/multilife/events/EventBusHandler.java
@@ -0,0 +1,11 @@
+package soze.multilife.events;
+
+import com.google.common.eventbus.EventBus;
+
+/**
+ * A class which provides no extension to EventBus, but implements EventHandler
+ * interface.
+ */
+public class EventBusHandler extends EventBus implements EventHandler {
+
+}
---
+++ server/src/main/java/soze/multilife/events/EventHandler.java
@@ -0,0 +1,32 @@
+package soze.multilife.events;
+
+/**
+ * An interface for event handlers. The interface follows the publish-subscribe pattern.
+ * Provides methods for (un)registering observers
+ * and posting events. Closely follows the EventBus (google guava)
+ * interface. This is not supposed to be an interface implemented by publishers
+ * or subscribers, but by an intermediate object - one which relays
+ * events from publishers to subscribers.
+ */
+public interface EventHandler {
+
+  /**
+   * Registers an observer. Same observer cannot be registered twice.
+   * @param observer
+   */
+  public void register(Object observer);
+
+  /**
+   * Unregisters an observer.
+   * @param observer
+   */
+  public void unregister(Object observer);
+
+  /**
+   * Posts an event. This event will be passed down to every
+   * observer subscribing to its type.
+   * @param event
+   */
+  public void post(Object event);
+
+}
