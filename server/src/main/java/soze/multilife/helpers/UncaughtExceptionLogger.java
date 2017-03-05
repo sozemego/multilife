b32675dd46da6c38dee335b89ae3e0393a1f073b
sozemego <piknypiec@gmail.com>
2017-03-05 21:16:14 +0100

    UncaughtExceptionLogger added for logging all uncaught exceptions
    Also minor comments

---
+++ server/src/main/java/soze/multilife/helpers/UncaughtExceptionLogger.java
@@ -0,0 +1,17 @@
+package soze.multilife.helpers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Logs uncaught exceptions thrown by threads.
+ */
+public class UncaughtExceptionLogger implements Thread.UncaughtExceptionHandler {
+
+  private static final Logger LOG = LoggerFactory.getLogger(UncaughtExceptionLogger.class);
+
+  @Override
+  public void uncaughtException(Thread t, Throwable e) {
+	LOG.error("Thread [{}], threw an exception [{}]", t, e);
+  }
+}
--- server/src/main/java/soze/multilife/server/InstanceFactory.java
+++ server/src/main/java/soze/multilife/server/InstanceFactory.java
@@ -8,9 +8,9 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 /**
- * Created by soze on 2/21/2017.
+ * An object responsible for creating, configuring and starting new {@link Instance} objects.
  */
-public class InstanceFactory {
+class InstanceFactory {
 
   private final Executor executor = Executors.newCachedThreadPool();
   private final Map<Long, Instance> instances;
@@ -18,7 +18,7 @@ public class InstanceFactory {
   private final SimulationFactory simulationFactory = new SimulationFactory();
   private long currentId = 0;
 
-  public InstanceFactory(Map<Long, Instance> instances) {
+  InstanceFactory(Map<Long, Instance> instances) {
 	this.instances = instances;
   }
 
@@ -26,10 +26,9 @@ public class InstanceFactory {
    * Finds an instance with free space, or creates a new one if unsuccesful.
    * For each new instance, a new {@link InstanceRunner} is created and started.
    *
-   * @return
+   * @return an instance
    */
-  public Instance getInstance() {
-
+  Instance getInstance() {
 
 	// first, try to find an instance with free space
 	synchronized (instances) {
@@ -42,7 +41,7 @@ public class InstanceFactory {
 
 	// not a single instance was found, so let's create a new one.
 	Simulation simulation = simulationFactory.getSimulation();
-	simulation.init();
+	simulation.init(); //TODO decide if this should be here
 	Instance instance = new Instance(++currentId, simulation, maxPlayers);
 	executor.execute(new InstanceRunner(instance));
 	return instance;
