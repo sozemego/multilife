79e0b01b43ddf96d8939b994b28e1e4d72e28954
sozemego <piknypiec@gmail.com>
2017-04-19 21:29:02 +0200

    Live display of average message size now works. needs prettying up

--- client/src/metrics.js
+++ client/src/metrics.js
@@ -23,30 +23,43 @@ export default class Metrics {
   _initCharts = () => {
     this.averageBytesChart = d3.select("#average-message-size")
 	  .append("svg")
-	  .attr("width", 400)
-	  .attr("height", 200)
-	  .append("g");
+	  .attr("width", 630)
+	  .attr("height", 420)
+	  .append("g")
+	  .attr("transform", "translate(30,20)");
 
-    let x = d3.scaleLinear().domain([0, 400]).range([0, 400]);
-	let y = d3.scaleLinear().domain([0, 50000]).range([0, 200]);
+    let x = d3.scaleLinear().domain([0, 600]).range([0, 600]);
+	let y = d3.scaleLinear().domain([0, 50000]).range([0, 400]);
 
     this.averageBytesChart.selectAll("line.x")
-	  .data(x.ticks(10))
+	  .data(x.ticks(5))
 	  .enter().append("line")
 	  .attr("x1", x)
 	  .attr("x2", x)
 	  .attr("y1", 0)
-	  .attr("y2", 200)
+	  .attr("y2", 400)
 	  .style("stroke", "#ccc");
 
 	this.averageBytesChart.selectAll("line.y")
-	  .data(y.ticks(10))
+	  .data(y.ticks(5))
 	  .enter().append("line")
 	  .attr("x1", 0)
-	  .attr("x2", 400)
+	  .attr("x2", 600)
 	  .attr("y1", y)
 	  .attr("y2", y)
 	  .style("stroke", "#ccc");
+
+	let yAxis = d3.axisLeft().scale(y);
+	this.averageBytesChart
+	  .append("g")
+	  .attr("class", "y axis")
+	  .call(yAxis);
+
+	let xAxis = d3.axisTop().scale(x);
+	this.averageBytesChart
+	  .append("g")
+	  .attr("class", "x axis")
+	  .call(xAxis);
   };
 
   _handleMessage = (msg) => {
@@ -57,7 +70,7 @@ export default class Metrics {
 
   _handleMetrics = (msg) => {
     this._displayBytesSent(msg.totalBytesSent);
-    this._displayAverageBytesChart(msg.averageBytesPerMessage);
+    this._displayAverageKbChart(msg.averageBytesPerMessage);
   };
 
   _displayBytesSent = (bytesSent) => {
@@ -72,30 +85,59 @@ export default class Metrics {
 
   };
 
-  _displayAverageBytesChart = (averageBytes) => {
-    this.averageBytes.push({kb: averageBytes, time: new Date()});
+  _displayAverageKbChart = (averageBytes) => {
+    this.averageBytes.push({kb: (averageBytes / 1000), time: new Date()});
 
-    let max = d3.max(this.averageBytes.map(d => d.bytes)) * 1.25;
+    let max = d3.max(this.averageBytes.map(d => d.kb)) * 1.25;
+    let minDate = d3.min(this.averageBytes.map(d => d.time));
+    let maxDate = d3.max(this.averageBytes.map(d => d.time));
 
-	let x = d3.scaleLinear().domain([0, 400]).range([0, 400]);
-	let y = d3.scaleLinear().domain([0, max]).range([0, 200]);
+    let timeDomainMin = this._getTimeDomainMin();
+    let timeDomainMax = this._getTimeDomainMax();
+
+	let x = d3.scaleTime().domain([timeDomainMin, timeDomainMax]).range([0, 600]);
+	let y = d3.scaleLinear().domain([0, max]).range([400, 0]);
 
 	let i = 0;
 
 	let line = d3.line()
-	  .x(d => x(i += 5))
-	  .y(d => y(max - d.bytes));
+	  .x(d => x(d.time))
+	  .y(d => y(d.kb));
 
-	this.averageBytesChart.exit().remove();
+	let path = this.averageBytesChart.selectAll("path.content")
+	  .data([this.averageBytes]);
 
-	this.averageBytesChart.selectAll("path")
-	  .data(this.averageBytes, data => data.bytes)
-	  .enter()
+	path.exit().remove();
+
+	path.enter()
 	  .append("path")
-	  .attr("d", line(this.averageBytes))
+	  .attr("class", "content")
 	  .style("stroke", "#ddaddd")
+	  .style("stroke-width", "2px")
 	  .style("fill", "transparent");
 
+	path.attr("d", line(this.averageBytes));
+
+	let yAxis = d3.axisLeft().scale(y);
+	this.averageBytesChart.selectAll("g.y.axis")
+	  .call(yAxis);
+
+	let xAxis = d3.axisTop().scale(x).ticks(4);
+	this.averageBytesChart.selectAll("g.x.axis")
+	  .call(xAxis);
+
+  };
+
+  _getTimeDomainMin() {
+    let date = new Date();
+    date.setMinutes(date.getMinutes() - 5);
+    return date;
+  }
+
+  _getTimeDomainMax() {
+	let date = new Date();
+	date.setMinutes(date.getMinutes() + 1);
+	return date;
   }
 
 }
