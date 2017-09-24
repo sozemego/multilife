e70327b506d1fa0efeb37ec63d0e200dca4d6ed2
sozemego <piknypiec@gmail.com>
2017-09-24 15:36:17 +0200

    Refactor

--- client/src/metrics/total-messages-metric.js
+++ client/src/metrics/total-messages-metric.js
@@ -1,35 +1,37 @@
-import {textD3} from "./text-d3";
+import {textD3} from './text-d3';
+import {assertIsNumber, assertIsObject} from '../assert';
 
-const sentTextFunction = (data) => {
-	return "Total bytes sent: "
-		+ data.bytesSent + " (" + getTruncatedKb(data.bytesSent)
-		+ "kB, " + getTruncatedMb(data.bytesSent) + "MB). "
-		+ "Total messages: " + data.messagesSent;
+const sentTextFunction = data => {
+	assertIsObject(data);
+	return `Total bytes sent: ${data.bytesSent} (${getTruncatedKb(data.bytesSent)} kB, ${getTruncatedMb(data.bytesSent)} MB).
+	 Total messages: ${data.messagesSent}`;
 };
 
-const receivedTextFunction = (data) => {
-	return "Total bytes received: "
-		+ data.bytesReceived + " (" + getTruncatedKb(data.bytesReceived)
-		+ "kB, " + getTruncatedMb(data.bytesReceived) + "MB). "
-		+ "Total messages: " + data.messagesReceived;
+const receivedTextFunction = data => {
+	assertIsObject(data);
+	return `Total bytes received: ${data.bytesReceived} (${getTruncatedKb(data.bytesReceived)} kB, ${getTruncatedMb(data.bytesReceived)} MB).
+	 Total messages: ${data.messagesReceived}`;
 };
 
-const getTruncatedKb = (bytes) => {
-	return ("" + (bytes / 1024)).substr(0, 4);
+const getTruncatedKb = bytes => {
+	assertIsNumber(bytes);
+	return ('' + (bytes / 1024)).substr(0, 4);
 };
 
-const getTruncatedMb = (bytes) => {
-	return ("" + ((bytes / 1024) / 1024)).substr(0, 4);
+const getTruncatedMb = bytes => {
+	assertIsNumber(bytes);
+	return ('' + ((bytes / 1024) / 1024)).substr(0, 4);
 };
 
 /**
  * Handles the display of total messages sent/received, but also
  * total bytes/kb/mb sent/received.
  */
-export const totalMessagesMetric = (socket) => {
+export const totalMessagesMetric = socket => {
+	assertIsObject(socket);
 
-	const sentText = textD3(document.getElementById("total-bytes-sent"), sentTextFunction);
-	const receivedText = textD3(document.getElementById("total-bytes-received"), receivedTextFunction);
+	const sentText = textD3(document.getElementById('total-bytes-sent'), sentTextFunction);
+	const receivedText = textD3(document.getElementById('total-bytes-received'), receivedTextFunction);
 
 	const handleTotalMessages = (msg) => {
 		sentText.update({
@@ -43,5 +45,4 @@ export const totalMessagesMetric = (socket) => {
 	};
 
 	socket.addObserver(handleTotalMessages);
-
 };
