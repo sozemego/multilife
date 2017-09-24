583d6b95e04cd05086e6f0361f5a29f345207312
sozemego <piknypiec@gmail.com>
2017-09-24 15:53:52 +0200

    Refactor

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,6 +1,7 @@
-import {assertIsArray, assertIsNumber, assertIsObject} from './assert';
+import {assertIsArray, assertIsNumber, assertIsObject, assertIsString} from './assert';
 
 export const throwError = message => {
+	assertIsString(message);
 	throw new Error(message);
 };
 
@@ -11,7 +12,7 @@ export const findMaxNumber = arr => {
 	assertIsArray(arr);
 	let max = Number.MIN_SAFE_INTEGER;
 	arr.forEach(i => {
-		if(i > max) max = i;
+		if (i > max) max = i;
 	});
 	return max;
 };
@@ -19,7 +20,7 @@ export const findMaxNumber = arr => {
 /**
  * Generates and returns a random color in the form of #HEX (e.g. #000000).
  */
-export const generateRandomColor = () => '#'+Math.floor(Math.random()*16777215).toString(16);
+export const generateRandomColor = () => '#' + Math.floor(Math.random() * 16777215).toString(16);
 
 /**
  * Generates a specified amount of random colors and returns an array of them.
@@ -28,7 +29,7 @@ export const generateRandomColor = () => '#'+Math.floor(Math.random()*16777215).
 export const generateRandomColors = amount => {
 	assertIsNumber(amount);
 	const colors = [];
-	for(let i = 0; i < amount; i++) {
+	for (let i = 0; i < amount; i++) {
 		colors.push(generateRandomColor());
 	}
 	return colors;
@@ -40,7 +41,7 @@ export const convertIntToHexColor = int => {
 	const b = int & 0xFF,
 		g = (int & 0xFF00) >>> 8,
 		r = (int & 0xFF0000) >>> 16;
-	return "rgb(" + [r, g, b].join(",") + ")";
+	return 'rgb(' + [r, g, b].join(',') + ')';
 };
 
 /**
