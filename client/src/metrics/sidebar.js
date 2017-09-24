b55b852616b75eb4e6d761612305bd7f23c16f69
sozemego <piknypiec@gmail.com>
2017-09-24 15:28:26 +0200

    Refactor

--- client/src/metrics/sidebar.js
+++ client/src/metrics/sidebar.js
@@ -1,41 +1,39 @@
-import React from "react";
-import {MenuItem, Paper} from "material-ui";
-import LineIcon from "./line-chart.svg";
-import BarChart from "./bar-chart-7.svg";
+import React from 'react';
+import {MenuItem, Paper} from 'material-ui';
+import LineIcon from './line-chart.svg';
+import BarChart from './bar-chart-7.svg';
 
 const styles = {
-	sidebar: {
-
-	},
+	sidebar: {},
 	sideBarIcon: {
-		width: "24px",
-		height: "24px"
+		width: '24px',
+		height: '24px'
 	},
 	sideBarItem: {
-		fontSize: "0.65em"
-	},
+		fontSize: '0.65em'
+	}
 };
 
 const sidebar = (props) =>
 	<Paper zDepth={2} style={styles.sidebar}>
 		<MenuItem onTouchTap={() => props.onNavigationClicked(0)}
 				  style={styles.sideBarItem}
-				  leftIcon={<img src={LineIcon} alt="Maxim Basinski, http://www.flaticon.com/authors/maxim-basinski"/>}>
+				  leftIcon={<img src={LineIcon} alt='Maxim Basinski, http://www.flaticon.com/authors/maxim-basinski'/>}>
 			Average kb/s
 		</MenuItem>
 		<MenuItem onTouchTap={() => props.onNavigationClicked(1)}
 				  style={styles.sideBarItem}
-				  leftIcon={<img src={BarChart} alt="Maxim Basinski, http://www.flaticon.com/authors/maxim-basinski"/>}>
+				  leftIcon={<img src={BarChart} alt='Maxim Basinski, http://www.flaticon.com/authors/maxim-basinski'/>}>
 			Outgoing type chart
 		</MenuItem>
 		<MenuItem onTouchTap={() => props.onNavigationClicked(2)}
 				  style={styles.sideBarItem}
-				  leftIcon={<img src={BarChart} alt="Maxim Basinski, http://www.flaticon.com/authors/maxim-basinski"/>}>
+				  leftIcon={<img src={BarChart} alt='Maxim Basinski, http://www.flaticon.com/authors/maxim-basinski'/>}>
 			Incoming type chart
 		</MenuItem>
 		<MenuItem onTouchTap={() => props.onNavigationClicked(3)}
 				  style={styles.sideBarItem}
-				  leftIcon={<img src={LineIcon} alt="Maxim Basinski, http://www.flaticon.com/authors/maxim-basinski"/>}>
+				  leftIcon={<img src={LineIcon} alt='Maxim Basinski, http://www.flaticon.com/authors/maxim-basinski'/>}>
 			Player count
 		</MenuItem>
 	</Paper>;
