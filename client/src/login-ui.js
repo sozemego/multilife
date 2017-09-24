3b6daa83b94173037dfdd1da54283d7d66a8a46b
sozemego <piknypiec@gmail.com>
2017-09-24 15:48:38 +0200

    Refactor

--- client/src/login-ui.js
+++ client/src/login-ui.js
@@ -1,8 +1,8 @@
-import {notify, on} from "./event-bus";
-import {LOGGED_IN, LOGIN} from "./events";
+import {notify, on} from './event-bus';
+import {LOGGED_IN, LOGIN} from './events';
 
 const login = () => {
-	const name = document.getElementById("name").value.trim();
+	const name = document.getElementById('name').value.trim();
 	if (!name) {
 		return;
 	}
@@ -11,7 +11,7 @@ const login = () => {
 };
 
 const destroyLogin = () => {
-	const loginElement = document.getElementById("login-container");
+	const loginElement = document.getElementById('login-container');
 	const parentOfLogin = loginElement.parentNode;
 	parentOfLogin.removeChild(loginElement);
 };
@@ -21,18 +21,18 @@ export const createLoginUi = () => {
 	const loginUi = {};
 
 	loginUi.createLoginView = () => {
-		const dom = document.getElementById("login");
+		const dom = document.getElementById('login');
 
-		const name = document.createElement("input");
-		name.setAttribute("id", "name");
-		name.setAttribute("placeholder", "Type your name");
+		const name = document.createElement('input');
+		name.setAttribute('id', 'name');
+		name.setAttribute('placeholder', 'Type your name');
 		dom.appendChild(name);
 		name.focus();
 
-		const button = document.createElement("button");
-		button.appendChild(document.createTextNode("ENTER!"));
-		button.classList.add("login-button");
-		button.addEventListener("click", login);
+		const button = document.createElement('button');
+		button.appendChild(document.createTextNode('ENTER!'));
+		button.classList.add('login-button');
+		button.addEventListener('click', login);
 
 		dom.appendChild(button);
 		on(LOGGED_IN, destroyLogin);
