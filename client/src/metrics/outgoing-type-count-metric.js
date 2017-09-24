9f8ac2839d4f15b304b7f7e75f1b45174caaa6d8
sozemego <piknypiec@gmail.com>
2017-09-24 15:25:43 +0200

    Refactor

--- client/src/metrics/outgoing-type-count-metric.js
+++ client/src/metrics/outgoing-type-count-metric.js
@@ -1,8 +1,10 @@
-import {barChart} from "./bar-chart";
+import {barChart} from './bar-chart';
+import {assertIsObject} from '../assert';
 
 export const outgoingTypeCountMetric = socket => {
+	assertIsObject(socket);
 
-	const chart = barChart(document.getElementById("message-type-count-outgoing"));
+	const chart = barChart(document.getElementById('message-type-count-outgoing'));
 
 	const handleTypeCount = ({outgoingTypeCount: typeCount} = msg) => {
 		chart.update(typeCount);
