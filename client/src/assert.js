a8c8ec41e575e5116180cbbdacea852afefc7936
sozemego <piknypiec@gmail.com>
2017-09-24 13:40:24 +0200

    Basic functions for asserting argument types.

---
+++ client/src/assert.js
@@ -0,0 +1,40 @@
+/**
+ * Contains basic functions which assert values of arguments
+ * are of correct type.
+ */
+
+import {throwError} from "./utils";
+
+/**
+ * Returns type of the argument. Credit to: https://github.com/MathieuTurcotte/node-precond/blob/master/lib/checks.js
+ * @param argument
+ * @returns {*}
+ */
+const typeOf = argument => {
+	const type = typeof argument;
+	if(type === 'object') {
+		if(!argument) {
+			return 'null';
+		} else if(argument instanceof Array) {
+			return 'array';
+		}
+	}
+	return type;
+};
+
+const assertType = expected => {
+	return (argument, message) => {
+		const type = typeOf(argument);
+
+		if(expected === type) {
+			return argument;
+		}
+
+		throwError(`${message}. Expected [${expected}], but got [${argument}]`);
+	};
+};
+
+export const assertIsArray = assertType('array');
+export const assertIsNumber = assertType('number');
+export const assertIsBoolean = assertType('boolean');
+export const assertIsFunction = assertType('function');
